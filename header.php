6f7ba15cd9661b6acdd11cef131c85f62891e734
K.kosuke <94692670+k3036@users.noreply.github.com>
2022-05-25 19:01:10 +0900

    Create header.php

---
+++ header.php
@@ -0,0 +1,37 @@
+<!DOCTYPE html>
+<html>
+
+<head>
+  <meta charset="UTF-8">
+  <meta http-equiv="X-UA-Compatible" content="IE=google">
+  <meta name="viewport" content="width = device - width=device-width, initial-scale = 1.0">
+  <title>form</title>
+  <link rel="stylesheet" href="form0css.php">
+</head>
+
+<body>
+  <div class=changeParentElement>
+
+    <div class=" class = headMenu1">
+
+      <a href="#AtTheBeginning" style=text-decoration:none;>始めに</a>
+      &nbsp;&nbsp;&nbsp;&nbsp;
+      <a href="#Next" style=text-decoration:none;>体験</a>
+      &nbsp;&nbsp;&nbsp;&nbsp;
+      <a href="" style=text-decoration:none;>お問い合わせ</a>
+      &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;
+      <!-- クリックしたらモーダルを表示するボタン -->
+      <a href="" style=text-decoration:none; class="followSpace">サインイン</a>
+      <!-- オーバーレイ -->
+      <div class="followOverlay"></div>
+      <!-- モーダルウィンドウ -->
+      <div class="followModal">
+        <p>似た動作だよん</p>
+      </div>
+
+    </div>
+
+  </div>
+</body>
+
+</html>
