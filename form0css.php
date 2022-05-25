81f4effd56d5f8e900acaabccfdc8f1db49b176a
K.kosuke <94692670+k3036@users.noreply.github.com>
2022-05-25 18:59:12 +0900

    Create form0css.php

---
+++ form0css.php
@@ -0,0 +1,769 @@
+p.header0{
+font-size: 20px ;
+color: white ;
+background-color: black ;
+text-align: center ;
+}
+.header{
+display: absolute;
+width: 100%;
+height: 65px;
+background-color : rgba(0,0,0,0.7); ; //////////////////追従ヘッダーの背景色/////////////////
+text-align : center;
+/position: fixed;/
+}
+.headMenu0{
+position : relative;
+text-align: left;
+margin-right : 220px ;
+}
+.headMenu1{
+position : relative;
+text-align: center;
+margin-top : -90px ;
+left : 150px ;
+}
+
+
+
+
+
+
+
+
+
+
+.header0{
+position:relative;
+top : -20px ;
+}
+
+p.header0{
+font-weight :bold ;
+line-height : 60px ;
+}
+.covid19{
+position : relative;
+top: 20px;
+}
+.header.covid19{
+position : relative;
+text-align : center;
+}
+a { color:white;
+font-size: 15px ;
+}/*新型コロナウイルスに対する取り組みの最新情報をご案内のリンク文字色*/
+.header{
+position:fixed;
+width: 100%;
+top : 0 ;
+z-index : 999 ;
+height : 80px ;
+padding : 0 24px ;
+
+display: none;
+}
+.header{
+font-size : 24px ;
+letter-spacing : 4px ;
+line-height : 80px ;
+}
+
+
+
+#page_top{
+width: 118px;
+height: 50px;
+position: fixed;
+right: 0;
+bottom: -50px;
+background: black;
+}
+.ExperienceMakingACafe{
+background-color : black ;
+position: relative ;
+top : -410px ;
+height : 650px ;
+width: 100%;
+margin : 0 auto ;
+}
+<?php
+//CSSとして使うPHPファイルの一番上に記述
+header('Content-Type: text/css;', 'charset=utf-8'); ?>
+@charset "utf-8";
+.covid19{
+position : relative;
+top: 20px;
+}
+.header.covid19{
+position : relative;
+text-align : center;
+}
+a { color:white;
+font-size: 15px ;
+}//新型コロナウイルスに対する取り組みの最新情報をご案内のリンク文字色//
+.waitingRoom{
+position : relative;
+margin:0 auto ;
+}
+.wr{
+opacity: 0.75;
+}
+.wr{
+position : relative;
+top :-40px ;
+}
+.createYourFavoriteSpace{
+position : absolute;
+top : 250px ;
+color : white ;
+text-align: left;
+margin-left : 66px ;
+font-size : 60px ;
+font-weight: bold;
+}
+
+.Logo0{
+position : absolute;
+top : 70px ;
+text-align: left;
+margin-left : 60px ;
+}
+
+.topMenu1{
+display: block;
+position: relative;
+text-align:center ;
+top : -770px
+}
+.topMenu0{
+display: block;
+position: relative;
+text-align:right ;
+top : -727px ;
+left : -100px ;
+}
+
+#hero{
+height : 100vh ;
+display : flex ;
+flex-direction : column;
+justify-content : center ;
+align-items : center ;
+position : relative ;
+}
+#hero h1{
+font-size : 36px ;
+font-weight : bold ;
+}
+#about{
+padding : 100px 24px ;
+}
+#menu{
+padding : 100px 24px ;
+}
+.ABC{
+color : white ;
+position : relative ;
+top:-20px ;
+}
+
+.JumpTop{
+text-align: center;
+}
+.title0{
+position: absolute;
+top : 825px;
+left : 145px ;
+font-weight: bold;
+font-size: 14px;
+}
+.title1{
+position: absolute;
+top : 920px;
+left : 145px ;
+font-weight: bold;
+font-size: 14px;
+}
+.title2{
+position: absolute;
+top : -80px;
+left : 145px ;
+font-weight: bold;
+font-size: 14px;
+}
+.title3{
+position: absolute;
+top : 15px;
+left : 145px ;
+font-weight: bold;
+font-size: 14px;
+}
+.fot0{
+position: relative ;
+top : -95px;
+right: -55px;
+border-radius: 10px; /* 角丸の指定 */
+}
+.font1{
+position: relative ;
+top : -169px;
+margin-left : 23% ;
+}
+.font2{
+position: relative ;
+top : -338px;
+margin-left : 45% ;
+}
+.font3{
+position: relative ;
+top : -509px;
+margin-left : 70% ;
+}
+
+
+sizeFixed{
+position: relative ;
+margin-bottom : 10% ;
+padding : 5% ;
+}
+
+
+.favoriteLocation {
+position: absolute ;
+font-weight: bold;
+font-size : 35px ;
+top : 1030px ;
+left : 10px ;
+}
+.allFont4 {
+display:flex
+position: absolute ;
+text-align:center;
+margin-left : -95% ;
+margin-top : -42% ;
+}
+.fontClassic{
+position: absolute ;
+top : -300px;
+text-align:center;
+margin-left : 45% ;
+margin-top : 14.8% ;
+}
+.fontClassicName{
+position: relative ;
+font-weight: bold;
+font-size: 35px;
+margin-right : 330px ;
+top : 370px ;
+left : 135px ;
+}
+.fot1-0{
+border-radius: 10px; /* 角丸の指定 */
+}
+.fontBar{
+position: absolute ;
+top : -350px;
+text-align:center;
+margin-left : 68.25% ;
+margin-top : 17% ;
+}
+.fontBarName{
+position: relative ;
+font-weight: bold;
+font-size: 35px;
+margin-left :40% ;
+margin-top : 13.6% ;
+}
+.fontCamp{
+position: absolute ;
+top : -300px;
+text-align:center;
+margin-left : 91.7% ;
+margin-top : 14.8% ;
+}
+.fontCampName{
+font-weight: bold;
+font-size: 35px;
+margin-left : 90% ;
+margin-top : -4.1% ;
+}
+
+.fontResort{
+position: absolute ;
+top : -300px;
+text-align:center;
+margin-left : 115% ;
+margin-top : 14.8% ;
+}
+.ResortName{
+font-weight: bold;
+font-size: 20px;
+margin-left :79% ;
+margin-top : -6.2% ;
+}
+
+
+
+
+
+
+.goToEats{
+position : relative ;
+top : -300px;
+text-align:center;
+margin-left : 0% ;
+margin-top : 29% ;
+width: 100% ;
+}
+.clearBox{
+position : relative ;
+background-color: white;
+opacity: 0.4;
+top: -600px;
+left : 7% ;
+font-size : 17px ;
+width : 500px ;
+height : 125px ;
+text-align: left;
+}
+.go{
+position: absolute;
+top : 1400px;
+left : 110px ;
+font-weight: bold;
+font-size: 40px ;
+}
+
+.useTheCampaign{
+position: relative;
+top : -30px;
+}
+
+.Experience{
+position: absolute ;
+top : 1750px ;
+left : 60px ;
+color: white ;
+font-size: 33px ;
+font-weight: bold;
+}
+
+.Expert{
+position: absolute ;
+top : 1820px ;
+right : 685px ;
+color: white ;
+font-weight: bold;
+font-size: 14px ;
+}
+.Size3{
+height: 420px ;
+width: 370px ;
+border-radius: 10px; /* 角丸の指定 */
+}
+
+.cooking3{
+position: absolute ;
+display:flex;
+top : 1855px ;
+right : 100px ;
+}
+.Cooking0{
+position: relative ;
+left : 10px ;
+}
+.Cooking1{
+position: relative ;
+left : 25px ;
+}
+.Cooking2{
+position: relative ;
+left : 40px ;
+}
+
+.CookWord3{
+position: absolute ;
+display:flex;
+top : 2300px ;
+left : 80px ;
+color: white ;
+}
+.CookWord0{
+position: relative ;
+right : 15px ;
+bottom : 25px ;
+font-weight: bold;
+font-size: 20px ;
+}
+.CookWord1{
+position: relative ;
+left : 270px ;
+bottom : 25px ;
+font-weight: bold;
+font-size: 20px ;
+}
+.CookWord2{
+position: relative ;
+left : 560px ;
+bottom : 25px ;
+font-weight: bold;
+font-size: 20px ;
+}
+.CookWord32{
+position: absolute ;
+display:flex;
+top : 2320px ;
+left : 65px ;
+color: white ;
+}
+.CookWord00{
+position: relative ;
+left : 0px ;
+top : -10px ;
+font-size: 15px ;
+}
+.CookWord11{
+position: relative ;
+left : 160px ;
+top : -10px ;
+font-size: 15px ;
+}
+.CookWord22{
+position: relative ;
+left : 323px ;
+top : -10px ;
+font-size: 15px ;
+}
+.HostsNationwide{
+position: absolute ;
+left : 55px ;
+top : 2410px ;
+font-size : 35px ;
+font-weight: bold;
+}
+
+
+
+.Business3{
+position: absolute ;
+display:flex;
+top : 2500px ;
+right : 100px ;
+}
+.Business{
+position: relative ;
+left : 10px ;
+}
+.Community{
+position: relative ;
+left : 25px ;
+}
+.EatingAround{
+position: relative ;
+left : 40px ;
+}
+.ThreeBusinessmen{
+position: absolute ;
+display:flex;
+top : 2930px ;
+left : 10px ;
+font-weight: bold;
+font-size:19px ;
+}
+.Size33{
+height: 250px ;
+width: 370px ;
+border-radius: 10px; /* 角丸の指定 */
+}
+.ThreeBusinessmen{
+position: absolute ;
+display:flex;
+top : 2760px ;
+left : 60px ;
+}
+.BusinessWord00{
+position: relative ;
+left : 10px ;
+}
+.CommunityWord11{
+position: relative ;
+left : 320px ;
+}
+.EatingAroundWord22{
+position: relative ;
+left : 590px ;
+}
+
+
+
+
+
+
+
+.FooterList{
+background-color : gray ;
+opacity: 0.1;
+position: relative ;
+top : 50px ;
+height : 500px ;
+width: 100%;
+margin : 0 auto ;
+}
+.footerWord{
+position: absolute ;
+text-align : left ;
+padding : 40px ;
+top : 3200px ;
+}
+footer{
+text-align : center ;
+padding : 10px ;
+}
+.MenuBelow{
+position: absolute ;
+display:flex;
+top : 2900px ;
+left : 60px ;
+font-weight: bold;
+}
+.{
+position: relative ;
+left : 30px ;
+}
+.CommunityImage{
+position: relative ;
+left : 25px ;
+}
+.Host{
+position: relative ;
+left : 370px ;
+}
+.Support{
+position: relative ;
+left : 615px ;
+}
+.Community{
+position: relative ;
+left : 190px ;
+}
+
+.FirstLineBelow{
+position: absolute ;
+display:flex;
+top : 2940px ;
+left : 30px ;
+}
+.Corporate{
+position: relative ;
+left : 30px ;
+}
+.Diversity{
+position: relative ;
+left : 205px ;
+}
+.XCharacter{
+position: relative ;
+left : 370px ;
+}
+.InitiativesForTheNewCoronavirus{
+position: relative ;
+left : 505px ;
+}
+.SecondLine{
+position: absolute ;
+display:flex;
+top : 2980px ;
+left : 30px ;
+}
+.NewsRoom{
+position: relative ;
+left : 30px ;
+}
+.AccessibilitySupport{
+position: relative ;
+left : 175px ;
+}
+.XCharacter2{
+position: relative ;
+left : 290px ;
+}
+.HelpCenter{
+position: relative ;
+left : 425px ;
+}
+.ThirdLine{
+position: absolute ;
+display:flex;
+top : 3020px ;
+left : 30px ;
+}
+.ToShareholdersAndInvestors{
+position: relative ;
+left : 30px ;
+}
+.InviteYourFriends{
+position: relative ;
+left : 95px ;
+}
+.XCharacter3{
+position: relative ;
+left : 275px ;
+}
+.CancellationOption{
+position: relative ;
+left : 410px ;
+}
+.FourLine{
+position: absolute ;
+display:flex;
+top : 3060px ;
+left : 30px ;
+}
+.XCharacter4{
+position: relative ;
+left : 30px ;
+}
+.XCharacter5{
+position: relative ;
+left : 125px ;
+}
+.CommunitySupport{
+position: relative ;
+left : 540px ;
+}
+.FiveLine{
+position: absolute ;
+display:flex;
+top : 3100px ;
+left : 30px ;
+}
+.XCharacter6{
+position: relative ;
+left : 30px ;
+}
+.XCharacter7{
+position: relative ;
+left : 125px ;
+}
+.TrustAndSafety{
+position: relative ;
+left : 540px ;
+}
+.SixLine{
+position: absolute ;
+display:flex;
+top : 3140px ;
+left : 60px ;
+}
+.XCharacter6{
+position: relative ;
+left : 30px ;
+}
+
+.EmploymentInformation{
+position: absolute ;
+display:flex;
+top : 3180px ;
+left : 88px ;
+}
+.Employment{
+position: relative ;
+right : 30px ;
+}
+.FooterLine{/*フッターのライン*/
+position: relative;
+border-top: 1px solid gray ;
+text-align: center;
+margin: 0 auto;
+bottom: 92px;
+border-width:1px;
+width : 92%;
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+.space:hover,
+.followSpace:hover {
+opacity: .5;
+}
+
+/* オーバーレイ 画面指定範囲を暗くする*/
+.overlay ,
+.followOverlay {
+/* 位置を固定 */
+position: fixed;
+top: 0;
+left: 0;
+/* 画面いっぱいに広がるようにする */
+width: 100%;
+height: 100vh;
+/* rgbaを使って60%の黒いオーバーレイにする */
+background: rgba(0, 0, 0, 0.6);
+/* デフォルトでは見えないようにする */
+opacity: 0;
+visibility: hidden;
+/* 表示する際の変化の所要時間 */
+transition: .10s;
+z-index : 888 ;
+}
+
+
+/* activeクラスのついたオーバーレイ */
+.overlay.active ,
+.followOverlay.active {
+/* activeクラスがついたときにオーバーレイを表示する */
+opacity: 1;
+visibility: visible;
+transform: translateY(0px);
+}
+
+
+
+/* モーダルウィンドウ */
+.modal,
+.followModal{
+text-align: center;
+max-width: 500px;
+width : 86%;
+height : 86%;
+/*padding: 15px 20px;*/
+background: white;
+/* 位置の調整(真ん中に表示) */
+position: fixed;
+/*モーダル表示位置*/
+top: 5%;
+left: 30%;
+transform: translate(0%, 100%);
+/* デフォルトでは非表示にしておく */
+opacity: 0;
+visibility: hidden;
+/* 表示の変化にかかる時間 */
+transition: .3s;
+z-index : 999 ;
+border-radius: 15px;
+}
+
+/* activeクラスのついたモーダルウィンドウ表示 */
+.modal.active ,
+.followModal.active{
+opacity: 1;
+visibility: visible;
+transform: translateX(0px);
+transition: all 1s;
+}
