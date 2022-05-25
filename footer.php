755048cb06fa40c4be89c688b704d86247057240
K.kosuke <94692670+k3036@users.noreply.github.com>
2022-05-25 19:02:01 +0900

    Create footer.php

---
+++ footer.php
@@ -0,0 +1,136 @@
+        <footer>
+          <div class="MenuBelow">
+
+
+            <p class=CorporateInformation>
+              企業情報
+            </p>
+
+            <p class=Community>
+              コミュニティ
+            </p>
+
+            <p class=Host>
+              ホスト
+            </p>
+
+            <p class=Support>
+              サポート
+            </p>
+
+          </div>
+
+          <div class="FirstLineBelow">
+
+
+            <p class=Corporate>
+              ご利用方法
+            </p>
+
+            <p class=Diversity>
+              ダイバーシティ
+            </p>
+
+            <p class=XCharacter>
+              XXXXXXXXXXXXXXX
+            </p>
+
+            <p class=InitiativesForTheNewCoronavirus>
+              新型コロナウイルスに対する取り組み
+            </p>
+
+          </div>
+
+          <div class="SecondLine">
+
+
+            <p class=NewsRoom>
+              ニュースルーム
+            </p>
+
+            <p class=AccessibilitySupport>
+              アクセシビリティ対応
+            </p>
+
+            <p class=XCharacter2>
+              XXXXXXXXXXXXXXX
+            </p>
+
+            <p class=HelpCenter>
+              ヘルプセンター
+            </p>
+
+          </div>
+
+          <div class="ThirdLine">
+
+
+            <p class=ToShareholdersAndInvestors>
+              株主・投資家のみなさまへ
+            </p>
+
+            <p class=InviteYourFriends>
+              お友達を招待
+            </p>
+
+            <p class=XCharacter3>
+              XXXXXXXXXXXXXXX
+            </p>
+
+            <p class=CancellationOption>
+              キャンセルオプション
+            </p>
+
+          </div>
+
+
+          <div class="FourLine">
+
+            <p class=XCharacter4>
+              XXXXXXXXXXXXXXX
+            </p>
+            <p class=XCharacter5>
+              XXXXXXXXXXXXXXX
+            </p>
+            <p class=CommunitySupport>
+              コミュニティサポート
+            </p>
+
+          </div>
+
+          <div class="FiveLine">
+
+            <p class=XCharacter6>
+              XXXXXXXXXXXXXXX
+            </p>
+            <p class=XCharacter7>
+              XXXXXXXXXXXXXXX
+            </p>
+            <p class=TrustAndSafety>
+              信頼＆安全
+            </p>
+
+          </div>
+          <div class="SixLine">
+
+            <p class=XCharacter8>
+              XXXXXXXXXXXXXXX
+            </p>
+
+          </div>
+          <div class="EmploymentInformation">
+
+            <p class=Employment>
+              採用情報
+            </p>
+
+          </div>
+          <div class="footerWord">
+            <small class=footerWord2>このサイトの素材は全て著作権フリーのものを使用しています。</small>
+            <br>
+            <small class=footerWord1>プライバシーポリシー 利用規約 サイトマップ 企業情報</small>
+            <br>
+            <small class=footerWord0>&copy;2021- LiNew, Inc. All rights reserved.</small>
+            <br>
+          </div>
+        </footer>
