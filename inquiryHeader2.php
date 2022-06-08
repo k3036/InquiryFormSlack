3abc7ce91faf38708c91ca759ba0ebcd9e3b10a6
K.kosuke <94692670+k3036@users.noreply.github.com>
2022-06-08 20:57:47 +0900

    Create inquiryHeader2.php

---
+++ inquiryHeader2.php
@@ -0,0 +1,69 @@
+<!DOCTYPE html>
+<html>
+
+<head>
+  <meta charset="UTF-8">
+  <meta http-equiv="X-UA-Compatible" content="IE=google">
+  <meta name="viewport" content="width = device - width=device-width, initial-scale = 1.0">
+  <title>inquiryHeader2.php</title>
+  <link rel="stylesheet" href="form0css.php">
+</head>
+
+
+<body>
+  <div class="headMenuSet">
+    <div class="headMenu5">
+      <a href="#"><img src="cafe\img\logo.png" alt="" width="210px" height="30px"></a>
+    </div>
+
+    <div class="topMenu3">
+      <p class="example">
+        <!-- クリックしたらモーダルを表示するボタン -->
+        <a href="" style=text-decoration:none; class="space">サインイン</a>
+        <a href="" style=text-decoration:none; class="followSpace"></a>
+        <!-- オーバーレイ -->
+      <div class="overlay"></div>
+      <!-- モーダルウィンドウ -->
+      <div class="modal">
+        <p class="Login2">ログイン</p>
+        <div class="modalLine3"> </div>
+        <div class="mailAddress2"> <input class="sizeTextBox2" type=" text" placeholder="パスワード"></div>
+        <div class="passWord2"> <input class="sizePassWord2" type=" text" placeholder="メールアドレス"></div>
+        <a href="" input type=" submit" class="submitButton2" style=text-decoration:none;>
+          <p class="sendSize2"> 送&nbsp;&nbsp;信</p>
+        </a>
+        <div class="modalLine4"> </div>
+        <a href="" input type=" submit" class="twitterButton2" style=text-decoration:none;>
+          <p class="markSize2"><img src="cafe\img\twitter.png" height="35px" width=35px top="35px"></p>
+        </a>
+        <a href="" input type=" submit" class="fbButton2" style=text-decoration:none;>
+          <p class="markSize2"><img src="cafe\img\fb.png" height="30px" width=30px></p>
+        </a>
+        <a href="" input type=" submit" class="googleButton2" style=text-decoration:none;>
+          <p class="markSize2"><img src="cafe\img\google.png" height="30px" width=30px></p>
+        </a>
+        <a href="" input type=" submit" class="appleButton2" style=text-decoration:none;>
+          <p class="markSize2"><img src="cafe\img\apple.png" height="30px" width=30px></p>
+        </a>
+      </div>
+    </div>
+    </p>
+    <p class="example2">
+      <a href="form0.php" style=text-decoration:none;>始めに</a> &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;
+      <a href=" form0.php " style=text-decoration:none;>体験</a> &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;
+      <a href="inquiryScreen.php" style=text-decoration:none;>お問い合わせ</a>
+    </p>
+  </div>
+
+  <!-- オーバーレイ -->
+  <div class="followOverlay"></div>
+  </div>
+  </div>
+
+  </div>
+  <script src="https://code.jquery.com/jquery-3.6.0.min.js"
+    integrity="sha256-/xUj+3OJU5yExlq6GSYGSHk7tPXikynS7ogEvDej/m4=" crossorigin="anonymous"></script>
+  <script src="form0main.js"></script>
+</body>
+
+</html>
