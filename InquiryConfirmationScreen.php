c877e066529b9b780dd948fd37cf3f118e2ec6c0
K.kosuke <94692670+k3036@users.noreply.github.com>
2022-06-19 21:42:23 +0900

    Update InquiryConfirmationScreen.php

--- InquiryConfirmationScreen.php
+++ InquiryConfirmationScreen.php
@@ -5,7 +5,7 @@
   <meta charset="UTF-8">
   <meta http-equiv="X-UA-Compatible" content="IE=google">
   <meta name="viewport" content="width = device - width=device-width, initial-scale = 1.0">
-  <title>InquiryConfirmationScreen.php</title>
+  <title>confirm.php</title>
   <link rel="stylesheet" href="form0css.php">
   <style type="text/css">
   body {
@@ -16,7 +16,9 @@
 </head>
 
 <body>
-  <?php include "inquiryHeader2.php";?>
+  <?php
+    include "inquiryHeader2.php";
+    ?>
 
 
   <main>
@@ -25,43 +27,72 @@
       <div class="inquiryLine"></div>
       <p class=Notes0>下記の内容をご確認の上送信ボタンを押してください</p>
       <p class=Notes1>内容を訂正する場合は戻るを押してください。</p>
-      <div class="Inquiry0LinesBelow"></div>
-      <div class="Inquiry1LinesBelow"></div>
-      <div class="Inquiry2LinesBelow"></div>
-      <div class="Inquiry3LinesBelow"></div>
-      <div class="Inquiry4LinesBelow"></div>
-      <p class="fillInYourName">氏名</p>
-      <p class="Confirmation0">山田太郎</p>
-      <p class="howToRead">フリガナ</p>
-      <p class="Confirmation1">ヤマダタロウ</p>
-      <p class="fillInThePhoneNumber">電話番号</p>
-      <p class="Confirmation2">09012345678</p>
-      <p class="fillInYourEmailAddress">メールアドレス</p>
-      <p class="Confirmation3">test@test.co.jp</p>
-      <p class="ContentsOfInquiry">お問い合わせ内容</p>
-      <p class="Confirmation4">Cafe-Cafe 御中</p>
 
 
 
-      <p class="ProgressConfirmationText1stLine">いつも大変お世話になっております。</p>
-      <p class="ProgressConfirmationText2stLine">株式会社〇〇の山田です。</p>
-      <P class="ProgressConfirmationText3stLine">先日メールにてお願いしておりましたXXの商品サンプルの件についてです</p>
-      <p class="ProgressConfirmationText4stLine">が、</p>
-      <p class="ProgressConfirmationText5stLine">その後の進捗は、いかがでしょうか。</p>
-      <p class="ProgressConfirmationText6stLine">当サンプルが必要となる会議が今週金曜日と迫っているため、本日15時ま</p>
-      <p class="ProgressConfirmationText7stLine">でにご送付をお願いできますでしょうか。</p>
-      <p class="ProgressConfirmationText8stLine">また、本メールと行き違いでご連絡をいただいておりましたら申し訳あり</p>
-      <p class="ProgressConfirmationText9stLine">ません。</p>
-      <p class="ProgressConfirmationText10stLine">お忙しいところ大変恐れ入りますが、お取り計らいの程、何卒よろしくお</p>
-      <p class="ProgressConfirmationText11stLine">願いいたします。</p>
+      <form method="POST" action="complete.php">
+        <?php function XssOCuntermeasure($XssMeasures){
+            echo htmlspecialchars($XssMeasures, ENT_QUOTES, "UTF-8") ;
+        }
+        ?>
+        <?php
+              /* データの受け取り */
+              $chineseCharacterName		= $_POST["chineseCharacterName"];		//お名前
+              $howToRead	= $_POST["howToRead"];        //読み仮名
+              $phoneNumber		= $_POST["phoneNumber"];  //電話番号
+              $emailAddress		= $_POST["emailAddress"];  //メールアドレス
+              $contentsOfInquiry		= $_POST["contentsOfInquiry"];//お問い合わせ内容
 
-      <a href="thankYouForYourInquiry.php" input type=" submit" class="submitButton4" style=text-decoration:none;
-        text-area="name=mutter;">
-        <p class="sendSize2"> 送&nbsp;&nbsp;信</p>
-      </a> <a href="inquiryScreen.php" input type=" submit" class="submitButton5" style=text-decoration:none;
-        text-area="name=mutter;">
-        <p class="sendSize2"> 戻&nbsp;&nbsp;る</p>
-      </a>
+              if($chineseCharacterName === ""){
+                echo "<FONT COLOR=\"RED\">※氏名は必須入力です。１０文字以内でご入力ください。</FONT>" ."</br>";
+              }
+              if($howToRead === ""){
+                echo"<FONT COLOR=\"RED\">※フリガナは必須入力です。１０文字以内でご入力ください。</FONT>"."</br>" ;
+              }
+              if($phoneNumber === ""){
+
+              }else if( !preg_match('/^0$|^-?[0-9][0-9]*$/', $phoneNumber)){
+                echo"<FONT COLOR=\"RED\">※電話番号は半角数字0-9のみでご入力ください。</FONT>" ."</br>";
+              }
+              if($emailAddress === ""){
+                echo"<FONT COLOR=\"RED\">※メールアドレスが正しく入力されておりません。</FONT>" ."</br>";
+              }
+              if($contentsOfInquiry === ""){
+                echo"<FONT COLOR=\"RED\">※お問い合わせ内容は必須入力です</FONT>" ;
+              }
+        ?>
+
+        <p class="fillInYourName">氏名</p>
+        <div class="Inquiry0LinesBelow"></div>
+        <div class="Confirmation0"><?php  XssOCuntermeasure($chineseCharacterName); ?>
+        </div>
+
+        <p class="fillInThePhoneNumber">フリガナ</p>
+        <div class="Inquiry1LinesBelow"></div>
+        <div class="Confirmation1"><?php XssOCuntermeasure($howToRead); ?></div>
+
+        <p class="fillInThePhoneNumber">電話番号</p>
+        <div class="Inquiry2LinesBelow"></div>
+        <div class="Confirmation2"><?php XssOCuntermeasure ($phoneNumber); ?></div>
+
+        <p class="fillInYourEmailAddress">メールアドレス</p>
+        <div class="Inquiry3LinesBelow"></div>
+        <div class="Confirmation3"><?php XssOCuntermeasure($emailAddress); ?></div>
+
+        <p class="ContentsOfInquiry">お問い合わせ内容</p>
+        <div class="Inquiry4LinesBelow"></div>
+        <div class="Confirmation4"><?php XssOCuntermeasure($contentsOfInquiry); ?></div>
+      </form>
+      <div class="sendOrBack">
+        <a href="complete.php" input type=" submit" class="submitButton4" style=text-decoration:none;
+          text-area="name=mutter;">
+          <p class="sendSize2"> 送&nbsp;&nbsp;信</p>
+        </a>
+        <a href="inquiryScreen.php" input type=" submit" class="submitButton5" style=text-decoration:none;
+          text-area="name=mutter;">
+          <p class="sendSize2"> 戻&nbsp;&nbsp;る</p>
+        </a>
+      </div>
 
     </div>
 
@@ -76,7 +107,9 @@
     <div class="footerMove"><?php include "footer.php";?>
     </div>
   </div>
-
+  <script src="https://code.jquery.com/jquery-3.6.0.min.js"
+    integrity="sha256-/xUj+3OJU5yExlq6GSYGSHk7tPXikynS7ogEvDej/m4=" crossorigin="anonymous"></script>
+  <script src="form0main.js"></script>
 </body>
 
 </html>
