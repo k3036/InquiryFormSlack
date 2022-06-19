3b9004f57da3994a81d3a63764e9d5b11c0be53c
K.kosuke <94692670+k3036@users.noreply.github.com>
2022-06-19 21:44:12 +0900

    Update form0main.js

--- form0main.js
+++ form0main.js
@@ -1,4 +1,13 @@
 
+function checkForm(){
+    if(document.form1.input01.value == "" || document.form1.input02.value == ""){
+        alert("必須項目を入力して下さい。");
+	return false;
+    }else{
+	return true;
+    }
+}
+
 jQuery(function() {
     var appear = false;
     var pageTop = $('#page_top');
